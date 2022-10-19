6fd58fdb951333a92fc16ef5e4b325aa24395291
chisomfrank0124 <ikechukuchisomfrank@gmail.com>
2022-10-18 20:35:31 -0700

    functions

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,20 +2,20 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- * main -> a function that prints _putchar
+ * main -> a main function
+ * Description: a function that prints _putchar
  * return: (void)
  */
 int main(void)
 {
-	//function definition
-	char c[8] ='_putchar';
+	/*function definition*/
+	char c[8] ="_putchar";
 	int i;
 
-	//logic
-	for(i = 0; i < 8; i++
+	/*logic*/
+	for(i = 0; i < 8; i++)
 	{
-	    _putchar(c[i]);
-        }    
+	       	_putchar(c[i]);
+        }
 	_putchar('\n');
-
-}	
+}
